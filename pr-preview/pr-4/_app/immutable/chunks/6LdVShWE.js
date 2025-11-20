be20627d087623e5f8b6eaa31ec31144c73cc123
theshortcut <theshortcut@users.noreply.github.com>
2025-11-20 04:10:45 +0000

    Deploy preview for PR 4 🛫

---
+++ pr-preview/pr-4/_app/immutable/chunks/6LdVShWE.js
@@ -0,0 +1,1 @@
+import{P as a}from"./Cjyocntl.js";a();
---
+++ pr-preview/pr-4/_app/immutable/chunks/C1JhY74o.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_rb9w6z?.base??"/clay.frrs.dev_new",e=globalThis.__sveltekit_rb9w6z?.assets??s??"";export{e as a,s as b};
